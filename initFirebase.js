bbd19a53a23633e6aa4dafab616d681736f620ff
adamazmil <muhd.adam1@gmail.com>
2021-03-18 14:56:45 -0500

    Update initFirebase.js
    Might not need this right now? Setup is done through npm currently, but in the future we many need this

--- initFirebase.js
+++ initFirebase.js
@@ -1,16 +1,18 @@
-import firebase from "firebase/app"
-import "firebase/auth";
-import "firebase/firestore";
+//Might not need this right now? Setup is done through npm currently, but in the future we many need this
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBxDI2F_zEe31DlR-dEC9K0rIQAyb4grhY",
-  authDomain: "fingerpaint-7b504.firebaseapp.com",
-  projectId: "fingerpaint-7b504",
-  storageBucket: "fingerpaint-7b504.appspot.com",
-  messagingSenderId: "490319202305",
-  appId: "1:490319202305:web:70af544e67c10ac31e9800",
-  measurementId: "G-3MQRDQBTS8"
-};
+// import firebase from "firebase/app"
+// import "firebase/auth";
+// import "firebase/firestore";
 
-firebase.initializeApp(firebaseConfig);
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBxDI2F_zEe31DlR-dEC9K0rIQAyb4grhY",
+//   authDomain: "fingerpaint-7b504.firebaseapp.com",
+//   projectId: "fingerpaint-7b504",
+//   storageBucket: "fingerpaint-7b504.appspot.com",
+//   messagingSenderId: "490319202305",
+//   appId: "1:490319202305:web:70af544e67c10ac31e9800",
+//   measurementId: "G-3MQRDQBTS8"
+// };
+
+// firebase.initializeApp(firebaseConfig);
