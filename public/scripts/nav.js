16a91446d09ca24601f274fb2fc092b6ed934bdb
adamazmil <muhd.adam1@gmail.com>
2021-03-26 10:39:37 -0500

    Button now usable instead of anchors

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -3,7 +3,7 @@ Fingerpaint.prototype.goToAccount = function(){
     window.location.href ="/account.html"
 }; 
 Fingerpaint.prototype.goToHome = function(){
-    
+
     window.location.href ="/home.html"
 }; 
 Fingerpaint.prototype.goToDraw = function(){
