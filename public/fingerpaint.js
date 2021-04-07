2d6b40034a7a418cd2947e70e18a524ef32bbd78
adamazmil <muhd.adam1@gmail.com>
2021-04-07 17:54:40 -0500

    rename scripts to src

---
+++ public/fingerpaint.js
@@ -0,0 +1,33 @@
+/**
+ * 
+ * 
+ * fingerpaint, the best fucking app eber. 
+ * 
+ * 
+ */
+'use strict';
+
+/**
+ * Initializes the Fingerpaint app.
+ */
+
+function Fingerpaint() {
+
+  var that = this;
+
+  firebase.auth().onAuthStateChanged(function (user) {
+    //assuming 'anonymous' is not allowed.
+
+    if (user != null) {//then user is not null, don't need to log in. 
+      //display homescreen of logged in user. 
+    } else {//user is null, has to login or continue as guest. 
+
+        //that.initsetup();  //displays login page.
+
+      }
+  });
+}
+
+window.onload = function () {
+  window.app = new Fingerpaint();
+};
