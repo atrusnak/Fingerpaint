9642148b95a60b3168e2af87a7896698ea3eb79f
adamazmil <muhd.adam1@gmail.com>
2021-04-09 16:03:19 -0500

    naming conventions to match react

--- jsx/drawings.js
+++ jsx/drawings.js
@@ -3,25 +3,25 @@ class Drawings extends React.Component {
 
     render(){
         return(
-            <table class="table table-striped table-inverse table-responsive">
-            <thead class="thead-inverse">
+            <table className="table table-striped table-inverse table-responsive">
+            <thead className="thead-inverse">
     
                 </thead>
     
                 <tbody>
                     <tr>
                         <td scope="row"></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw1.jpg" width="150" height="150"/></a></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw2.jpg" width="150" height="150"/></a></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw3.jpg" width="150" height="150"/></a></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw4.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw1.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw2.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw3.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw4.jpg" width="150" height="150"/></a></td>
                     </tr>
                     <tr>
                         <td scope="row"></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw4.jpg" width="150" height="150"/></a></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw3.jpg" width="150" height="150"/></a></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw2.jpg" width="150" height="150"/></a></td>
-                        <td><a id="profile" onclick = "Fingerpaint.prototype.goToDraw()"><img src="draw1.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw4.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw3.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw2.jpg" width="150" height="150"/></a></td>
+                        <td><a id="profile" onClick = {() => Fingerpaint.prototype.goToDraw()}><img src="draw1.jpg" width="150" height="150"/></a></td>
                     </tr>
                 </tbody>
         </table>
