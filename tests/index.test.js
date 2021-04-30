284357ea978fee891ffee36c7ce902e1b1d6f9eb
adamazmil <muhd.adam1@gmail.com>
2021-04-30 16:39:20 -0500

    2nd test login

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,4 +6,14 @@ describe('Fingerpaint', () => {
     it('should be titled "Fingerpaint"', async () => {
       await expect(page.title()).resolves.toMatch('Fingerpaint');
     });
+
+    it('on login should go to home', async ()=>{
+      await page.type('#inputEmail','muhd.adam1@gmail.com');
+      await page.type('#inputPassword','newpassword');
+      await page.click('button', { text: 'Sign in' });
+      await page.waitForNavigation();
+      await expect(page.title()).resolves.toMatch('Home');
+    });
+
+    
   });
