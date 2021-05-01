de9130bf50108ccf253a2209d5d00c8eaa272054
adamazmil <muhd.adam1@gmail.com>
2021-04-30 19:53:20 -0500

    some inconsistencies fixed due to time outs

--- tests/logout.test.js
+++ tests/logout.test.js
@@ -1,20 +1,19 @@
 describe('Logout', () => {
+  //jest.setTimeout(20000);
   beforeAll(async () => {
     await page.goto('http://localhost:5002/index.html');
-    //jest.setTimeout(20000);
     await page.type('#inputEmail','muhd.adam1@gmail.com');
     await page.type('#inputPassword','newpassword');
     await page.click('button', { text: 'Sign in' });
     await page.waitForNavigation();
     await page.goto('http://localhost:5002/account.html');
-    await page.waitForNavigation();
-  });
+  },10000);
 
   it('on logout title should be Fingerpaint', async ()=>{
 
-     await page.waitForSelector('#logout');
-     await page.click('#logout');
-     await page.waitForNavigation();
+      await page.waitForSelector('#logout');
+      await page.click('#logout');
+      await page.waitForNavigation();
      await expect(page.title()).resolves.toMatch('Fingerpaint');
-  });
+  },10000);
 });
